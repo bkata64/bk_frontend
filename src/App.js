85f45d40fb539adf5488427091f6601d4e096d0e
bkata64 <bkata64@gmail.com>
2023-03-13 11:17:35 +0100

    Indul

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import OpenPage from './Components/OpenPage';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import Offers from './Components/Offers';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={<OpenPage/>}/>
+        <Route exact path="/offers" element={<Offers/>}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
